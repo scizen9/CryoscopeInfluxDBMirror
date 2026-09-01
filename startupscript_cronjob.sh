7902102fded0b1cba3cf6679d8da1f5eca38503b
Don Neill <neilljd@gmail.com>
2026-09-01 09:10:07 -0700

    Add startupscript_cronjob.sh to ensure mirroring scripts are running.

---
+++ startupscript_cronjob.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# startupscript_cronjob.sh
+#
+# This script is meant to be run as a cron job to make sure
+# that the mirroring script is running.
+#
+# Example crontab entry that runs this script every minute
+# (modify for the correct directory):
+# */1 * * * * $HOME/CryoscopeInfluxDBMirror/startupscript_cronjob.sh > /dev/null 2>&1
+#
+# DIRECTORIES Set Up, PLEASE EDIT:
+mirrordir=$HOME/CryoscopeInfluxDBMirror
+#
+ret=`pgrep -f CryoscopeInfluxDBMirror | wc -l`
+if [ $ret -lt 1 ]; then
+      cd $tooldir
+      # Use the line below instead to enable screen logging
+      # screen -L -dmS CryoscopeInfluxDBMirror python3 main.py forceOn
+      screen -dmS CryoscopeInfluxDBMirror python3 ./main.py forceOn
+fi
