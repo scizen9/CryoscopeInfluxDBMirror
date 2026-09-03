0b92044f2f32ab71cbc1731c008a167e7724d366
Don Neill <neilljd@gmail.com>
2026-09-02 17:39:04 -0700

    Fix startupscript_cronjob.sh

--- startupscript_cronjob.sh
+++ startupscript_cronjob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -u
 # startupscript_cronjob.sh
 #
 # This script is meant to be run as a cron job to make sure
@@ -10,12 +10,12 @@
 # */1 * * * * $HOME/CryoscopeInfluxDBMirror/startupscript_cronjob.sh > /dev/null 2>&1
 #
 # DIRECTORIES Set Up, PLEASE EDIT:
-mirrordir=$HOME/CryoscopeInfluxDBMirror
+mirrordir="$HOME/CryoscopeInfluxDBMirror"
 #
-ret=`pgrep -f CryoscopeInfluxDBMirror | wc -l`
+ret=`pgrep -f cryoscope_db_mirror | wc -l`
 if [ $ret -lt 1 ]; then
       cd $mirrordir
       # Use the line below instead to enable screen logging
       # screen -L -dmS CryoscopeInfluxDBMirror python3 main.py forceOn
-      screen -dmS CryoscopeInfluxDBMirror python3 ./main.py forceOn
+      screen -dmS cryoscope_db_mirror ./main.py forceOn
 fi
